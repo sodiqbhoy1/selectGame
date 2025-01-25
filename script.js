37c20676ed008a4a4c8b32d0298873e073780326
Sodiqbhoy1 <adamubabasodiq@gmail.com>
2025-01-25 16:12:11 +0100

    Refactor submit button logic for concise value selection feedback

--- script.js
+++ script.js
@@ -19,35 +19,11 @@ Allbuttons.forEach(button =>{
        
     })
 })
-
-
 submitButton.addEventListener('click', ()=>{
 
-    if(selectedValue){
-        output.textContent = `
-         <p> You selected ${selectedValue} out 5 </p>
-        `
-    } else{
-        output.textContent = `
-        <p> Please select a value </p>
-
-       `
-        
-
-    }
-
+    selectedValue ?  output.textContent = `
+    <p> You selected ${selectedValue} out 5 </p>
+   `
+   :  output.textContent = `
+   <p> Please select a value </p> `
 })
-        
-//         // Store the value of the selected button
-//         selectedValue = button.getAttribute('data-value');
-//     });
-// });
-
-// // Submit button logic
-// submitButton.addEventListener('click', function() {
-//     if (selectedValue) {
-//         alert(`You selected button with value: ${selectedValue}`);
-//     } else {
-//         alert('Please select a button first!');
-//     }
-// });
